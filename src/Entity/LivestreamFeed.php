711e8aebe5fe6bba1aa384eacb110596da77d491
Christopher C. Wells <git@chris-wells.net>
2021-11-14 21:01:14 -0800

    Update property type declarations for nullables

--- src/Entity/LivestreamFeed.php
+++ src/Entity/LivestreamFeed.php
@@ -15,239 +15,141 @@ use DateTime;
 class LivestreamFeed extends EntityBase
 {
     private string $profile;
-    private string $playerCode;
+    private ?string $playerCode = null;
     private string $cid;
     private DateTime $createdAt;
     private DateTime $updatedAt;
-    private string $associatedTvssFeed;
-    private string $nonDrmUrl;
-    private string $drmHlsUrl;
-    private string $drmDashUrl;
-    private string $fairplayLicense;
-    private string $fairplayCertificate;
-    private string $widevineLicense;
-    private string $playreadyLicense;
+    private ?string $associatedTvssFeed = null;
+    private ?string $nonDrmUrl = null;
+    private ?string $drmHlsUrl = null;
+    private ?string $drmDashUrl = null;
+    private ?string $fairplayLicense = null;
+    private ?string $fairplayCertificate = null;
+    private ?string $widevineLicense = null;
+    private ?string $playreadyLicense = null;
     private bool $logoOverlay;
 
-    /**
-     * @return string
-     */
     public function getProfile(): string
     {
         return $this->profile;
     }
-
-    /**
-     * @param string $profile
-     */
     public function setProfile(string $profile): void
     {
         $this->profile = $profile;
     }
 
-    /**
-     * @return string
-     */
-    public function getPlayerCode(): string
+    public function getPlayerCode(): ?string
     {
         return $this->playerCode;
     }
-
-    /**
-     * @param string $playerCode
-     */
     public function setPlayerCode(string $playerCode): void
     {
         $this->playerCode = $playerCode;
     }
 
-    /**
-     * @return string
-     */
     public function getCid(): string
     {
         return $this->cid;
     }
-
-    /**
-     * @param string $cid
-     */
     public function setCid(string $cid): void
     {
         $this->cid = $cid;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getCreatedAt(): DateTime
     {
         return $this->createdAt;
     }
-
-    /**
-     * @param \DateTime $createdAt
-     */
     public function setCreatedAt(DateTime $createdAt): void
     {
         $this->createdAt = $createdAt;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getUpdatedAt(): DateTime
     {
         return $this->updatedAt;
     }
-
-    /**
-     * @param \DateTime $updatedAt
-     */
     public function setUpdatedAt(DateTime $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
     }
 
-    /**
-     * @return string
-     */
-    public function getAssociatedTvssFeed(): string
+    public function getAssociatedTvssFeed(): ?string
     {
         return $this->associatedTvssFeed;
     }
-
-    /**
-     * @param string $associatedTvssFeed
-     */
     public function setAssociatedTvssFeed(string $associatedTvssFeed): void
     {
         $this->associatedTvssFeed = $associatedTvssFeed;
     }
 
-    /**
-     * @return string
-     */
-    public function getNonDrmUrl(): string
+    public function getNonDrmUrl(): ?string
     {
         return $this->nonDrmUrl;
     }
-
-    /**
-     * @param string $nonDrmUrl
-     */
     public function setNonDrmUrl(string $nonDrmUrl): void
     {
         $this->nonDrmUrl = $nonDrmUrl;
     }
 
-    /**
-     * @return string
-     */
-    public function getDrmHlsUrl(): string
+    public function getDrmHlsUrl(): ?string
     {
         return $this->drmHlsUrl;
     }
-
-    /**
-     * @param string $drmHlsUrl
-     */
     public function setDrmHlsUrl(string $drmHlsUrl): void
     {
         $this->drmHlsUrl = $drmHlsUrl;
     }
 
-    /**
-     * @return string
-     */
-    public function getDrmDashUrl(): string
+    public function getDrmDashUrl(): ?string
     {
         return $this->drmDashUrl;
     }
-
-    /**
-     * @param string $drmDashUrl
-     */
     public function setDrmDashUrl(string $drmDashUrl): void
     {
         $this->drmDashUrl = $drmDashUrl;
     }
 
-    /**
-     * @return string
-     */
-    public function getFairplayLicense(): string
+    public function getFairplayLicense(): ?string
     {
         return $this->fairplayLicense;
     }
-
-    /**
-     * @param string $fairplayLicense
-     */
     public function setFairplayLicense(string $fairplayLicense): void
     {
         $this->fairplayLicense = $fairplayLicense;
     }
 
-    /**
-     * @return string
-     */
-    public function getFairplayCertificate(): string
+    public function getFairplayCertificate(): ?string
     {
         return $this->fairplayCertificate;
     }
-
-    /**
-     * @param string $fairplayCertificate
-     */
     public function setFairplayCertificate(string $fairplayCertificate): void
     {
         $this->fairplayCertificate = $fairplayCertificate;
     }
 
-    /**
-     * @return string
-     */
-    public function getWidevineLicense(): string
+    public function getWidevineLicense(): ?string
     {
         return $this->widevineLicense;
     }
-
-    /**
-     * @param string $widevineLicense
-     */
     public function setWidevineLicense(string $widevineLicense): void
     {
         $this->widevineLicense = $widevineLicense;
     }
 
-    /**
-     * @return string
-     */
-    public function getPlayreadyLicense(): string
+    public function getPlayreadyLicense(): ?string
     {
         return $this->playreadyLicense;
     }
-
-    /**
-     * @param string $playreadyLicense
-     */
     public function setPlayreadyLicense(string $playreadyLicense): void
     {
         $this->playreadyLicense = $playreadyLicense;
     }
 
-    /**
-     * @return bool
-     */
     public function isLogoOverlay(): bool
     {
         return $this->logoOverlay;
     }
-
-    /**
-     * @param bool $logoOverlay
-     */
     public function setLogoOverlay(bool $logoOverlay): void
     {
         $this->logoOverlay = $logoOverlay;
--- src/Entity/Station.php
+++ src/Entity/Station.php
@@ -17,48 +17,48 @@ class Station extends EntityBase
     private string $id;
     private string $callSign;
     private string $fullCommonName;
-    private string $shortCommonName;
-    private string $tagLine;
+    private ?string $shortCommonName = null;
+    private ?string $tagLine = null;
     private string $timezone;
-    private string $secondaryTimezone;
-    private bool $passportEnabled;
-    private float $annualPassportQualifyingAmount;
+    private ?string $secondaryTimezone = null;
+    private ?bool $passportEnabled = null;
+    private ?float $annualPassportQualifyingAmount = null;
     private string $primaryChannel;
     private string $primetimeStart;
-    private string $tvssUrl;
-    private string $donateUrl;
-    private string $kidsLiveStreamUrl;
-    private string $websiteUrl;
-    private string $learnMorePassportUrl;
-    private string $facebookUrl;
-    private string $twitterUrl;
-    private string $stationKidsUrl;
-    private string $passportUrl;
-    private string $videoPortalUrl;
-    private string $videoPortalBannerUrl;
+    private ?string $tvssUrl = null;
+    private ?string $donateUrl = null;
+    private ?string $kidsLiveStreamUrl = null;
+    private ?string $websiteUrl = null;
+    private ?string $learnMorePassportUrl = null;
+    private ?string $facebookUrl = null;
+    private ?string $twitterUrl = null;
+    private ?string $stationKidsUrl = null;
+    private ?string $passportUrl = null;
+    private ?string $videoPortalUrl = null;
+    private ?string $videoPortalBannerUrl = null;
     private bool $pdp;
-    private string $addressLine1;
-    private string $addressLine2;
-    private string $city;
-    private string $state;
-    private string $zipCode;
-    private string $countryCode;
-    private string $email;
-    private string $telephone;
-    private string $fax;
+    private ?string $addressLine1 = null;
+    private ?string $addressLine2 = null;
+    private ?string $city = null;
+    private ?string $state = null;
+    private ?string $zipCode = null;
+    private ?string $countryCode = null;
+    private ?string $email = null;
+    private ?string $telephone = null;
+    private ?string $fax = null;
     private array $images;
-    private string $pageTracking;
-    private string $eventTracking;
+    private ?string $pageTracking = null;
+    private ?string $eventTracking = null;
     private DateTime $updatedAt;
-    private string $gaLiveStreamUrl;
+    private ?string $gaLiveStreamUrl = null;
     private bool $displayLogoOverlay;
-    private string $gaLiveStreamFeedCid;
-    private string $playerCode;
+    private ?string $gaLiveStreamFeedCid = null;
+    private ?string $playerCode = null;
 
     /**
      * @var \OpenPublicMedia\PbsStationManager\Entity\LivestreamFeed[]
      */
-    private array $livestreamFeeds;
+    private array $livestreamFeeds = [];
 
     /**
      * Base entity constructor.
@@ -75,653 +75,363 @@ class Station extends EntityBase
         parent::__construct($apiAttributes);
     }
 
-    /**
-     * @return string
-     */
     public function getId(): string
     {
         return $this->id;
     }
-
-    /**
-     * @param string $id
-     */
     public function setId(string $id)
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getCallSign(): string
     {
         return $this->callSign;
     }
-
-    /**
-     * @param string $callSign
-     */
     public function setCallSign(string $callSign)
     {
         $this->callSign = $callSign;
     }
 
-    /**
-     * @return string
-     */
     public function getFullCommonName(): string
     {
         return $this->fullCommonName;
     }
-
-    /**
-     * @param string $fullCommonName
-     */
     public function setFullCommonName(string $fullCommonName)
     {
         $this->fullCommonName = $fullCommonName;
     }
 
-    /**
-     * @return string|null
-     */
     public function getShortCommonName(): ?string
     {
         return $this->shortCommonName;
     }
-
-    /**
-     * @param string|null $shortCommonName
-     */
-    public function setShortCommonName(?string $shortCommonName)
+    public function setShortCommonName(string $shortCommonName)
     {
         $this->shortCommonName = $shortCommonName;
     }
 
-    /**
-     * @return string|null
-     */
     public function getTagLine(): ?string
     {
         return $this->tagLine;
     }
-
-    /**
-     * @param string $tagLine
-     */
     public function setTagLine(string $tagLine)
     {
         $this->tagLine = $tagLine;
     }
 
-    /**
-     * @return string
-     */
     public function getTimezone(): string
     {
         return $this->timezone;
     }
-
-    /**
-     * @param string $timezone
-     */
     public function setTimezone(string $timezone)
     {
         $this->timezone = $timezone;
     }
 
-    /**
-     * @return string|null
-     */
     public function getSecondaryTimezone(): ?string
     {
         return $this->secondaryTimezone;
     }
 
-    /**
-     * @param string|null $secondaryTimezone
-     */
-    public function setSecondaryTimezone(?string $secondaryTimezone)
+    public function setSecondaryTimezone(string $secondaryTimezone)
     {
         $this->secondaryTimezone = $secondaryTimezone;
     }
 
-    /**
-     * Internal API only.
-     *
-     * @return bool|null
-     */
     public function isPassportEnabled(): ?bool
     {
         return $this->passportEnabled;
     }
-
-    /**
-     * @param bool|null $passportEnabled
-     *   NULL indicates _not provided_ (e.g. when querying the public API).
-     */
-    public function setPassportEnabled(?bool $passportEnabled)
+    public function setPassportEnabled(bool $passportEnabled)
     {
         $this->passportEnabled = $passportEnabled;
     }
 
-    /**
-     * Internal API only.
-     *
-     * @return float|null
-     */
     public function getAnnualPassportQualifyingAmount(): ?float
     {
         return $this->annualPassportQualifyingAmount;
     }
-
-    /**
-     * @param float|null $annualPassportQualifyingAmount
-     *   NULL indicates _not provided_ (e.g. when querying the public API).
-     */
-    public function setAnnualPassportQualifyingAmount(?float $annualPassportQualifyingAmount)
+    public function setAnnualPassportQualifyingAmount(float $annualPassportQualifyingAmount)
     {
         $this->annualPassportQualifyingAmount = $annualPassportQualifyingAmount;
     }
 
-    /**
-     * @return string
-     */
     public function getPrimaryChannel(): string
     {
         return $this->primaryChannel;
     }
-
-    /**
-     * @param string $primaryChannel
-     */
     public function setPrimaryChannel(string $primaryChannel)
     {
         $this->primaryChannel = $primaryChannel;
     }
 
-    /**
-     * @return string|null
-     */
     public function getPrimetimeStart(): ?string
     {
         return $this->primetimeStart;
     }
-
-    /**
-     * @param string|null $primetimeStart
-     */
-    public function setPrimetimeStart(?string $primetimeStart)
+    public function setPrimetimeStart(string $primetimeStart)
     {
         $this->primetimeStart = $primetimeStart;
     }
 
-    /**
-     * @return string|null
-     */
     public function getTvssUrl(): ?string
     {
         return $this->tvssUrl;
     }
-
-    /**
-     * @param string|null $tvssUrl
-     */
-    public function setTvssUrl(?string $tvssUrl)
+    public function setTvssUrl(string $tvssUrl)
     {
         $this->tvssUrl = $tvssUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getDonateUrl(): ?string
     {
         return $this->donateUrl;
     }
-
-    /**
-     * @param string|null $donateUrl
-     */
-    public function setDonateUrl(?string $donateUrl)
+    public function setDonateUrl(string $donateUrl)
     {
         $this->donateUrl = $donateUrl;
     }
 
-    /**
-     * Internal API only.
-     *
-     * @return string|null
-     */
     public function getKidsLiveStreamUrl(): ?string
     {
         return $this->kidsLiveStreamUrl;
     }
-
-    /**
-     * @param string|null $kidsLiveStreamUrl
-     *   NULL indicates _not provided_ (e.g. when querying the public API).
-     */
-    public function setKidsLiveStreamUrl(?string $kidsLiveStreamUrl)
+    public function setKidsLiveStreamUrl(string $kidsLiveStreamUrl)
     {
         $this->kidsLiveStreamUrl = $kidsLiveStreamUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getWebsiteUrl(): ?string
     {
         return $this->websiteUrl;
     }
-
-    /**
-     * @param string|null $websiteUrl
-     */
-    public function setWebsiteUrl(?string $websiteUrl)
+    public function setWebsiteUrl(string $websiteUrl)
     {
         $this->websiteUrl = $websiteUrl;
     }
 
-    /**
-     * @return string
-     */
-    public function getLearnMorePassportUrl(): string
+    public function getLearnMorePassportUrl(): ?string
     {
         return $this->learnMorePassportUrl;
     }
-
-    /**
-     * @param string $learnMorePassportUrl
-     */
     public function setLearnMorePassportUrl(string $learnMorePassportUrl): void
     {
         $this->learnMorePassportUrl = $learnMorePassportUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getFacebookUrl(): ?string
     {
         return $this->facebookUrl;
     }
-
-    /**
-     * @param string|null $facebookUrl
-     */
     public function setFacebookUrl(?string $facebookUrl)
     {
         $this->facebookUrl = $facebookUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getTwitterUrl(): ?string
     {
         return $this->twitterUrl;
     }
-
-    /**
-     * @param string|null $twitterUrl
-     */
-    public function setTwitterUrl(?string $twitterUrl)
+    public function setTwitterUrl(string $twitterUrl)
     {
         $this->twitterUrl = $twitterUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getStationKidsUrl(): ?string
     {
         return $this->stationKidsUrl;
     }
-
-    /**
-     * @param string|null $stationKidsUrl
-     */
-    public function setStationKidsUrl(?string $stationKidsUrl)
+    public function setStationKidsUrl(string $stationKidsUrl)
     {
         $this->stationKidsUrl = $stationKidsUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getPassportUrl(): ?string
     {
         return $this->passportUrl;
     }
-
-    /**
-     * @param string|null $passportUrl
-     */
-    public function setPassportUrl(?string $passportUrl)
+    public function setPassportUrl(string $passportUrl)
     {
         $this->passportUrl = $passportUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getVideoPortalUrl(): ?string
     {
         return $this->videoPortalUrl;
     }
-
-    /**
-     * @param string|null $videoPortalUrl
-     */
-    public function setVideoPortalUrl(?string $videoPortalUrl)
+    public function setVideoPortalUrl(string $videoPortalUrl)
     {
         $this->videoPortalUrl = $videoPortalUrl;
     }
 
-    /**
-     * @return string|null
-     */
     public function getVideoPortalBannerUrl(): ?string
     {
         return $this->videoPortalBannerUrl;
     }
-
-    /**
-     * @param string|null $videoPortalBannerUrl
-     */
-    public function setVideoPortalBannerUrl(?string $videoPortalBannerUrl)
+    public function setVideoPortalBannerUrl(string $videoPortalBannerUrl)
     {
         $this->videoPortalBannerUrl = $videoPortalBannerUrl;
     }
 
-    /**
-     * Internal API only.
-     *
-     * @return bool|null
-     */
     public function isPdp(): ?bool
     {
         return $this->pdp;
     }
-
-    /**
-     * @param bool|null $pdp
-     *   NULL indicates _not provided_ (e.g. when querying the public API).
-     */
-    public function setPdp(?bool $pdp)
+    public function setPdp(bool $pdp)
     {
         $this->pdp = $pdp;
     }
 
-    /**
-     * @return string|null
-     */
     public function getAddressLine1(): ?string
     {
         return $this->addressLine1;
     }
-
-    /**
-     * @param string|null $addressLine1
-     */
-    public function setAddressLine1(?string $addressLine1)
+    public function setAddressLine1(string $addressLine1)
     {
         $this->addressLine1 = $addressLine1;
     }
 
-    /**
-     * @return string|null
-     */
     public function getAddressLine2(): ?string
     {
         return $this->addressLine2;
     }
-
-    /**
-     * @param string|null $addressLine2
-     */
-    public function setAddressLine2(?string $addressLine2)
+    public function setAddressLine2(string $addressLine2)
     {
         $this->addressLine2 = $addressLine2;
     }
 
-    /**
-     * @return string|null
-     */
     public function getCity(): ?string
     {
         return $this->city;
     }
-
-    /**
-     * @param string|null $city
-     */
-    public function setCity(?string $city)
+    public function setCity(string $city)
     {
         $this->city = $city;
     }
 
-    /**
-     * @return string|null
-     */
     public function getState(): ?string
     {
         return $this->state;
     }
-
-    /**
-     * @param string|null $state
-     */
-    public function setState(?string $state)
+    public function setState(string $state)
     {
         $this->state = $state;
     }
 
-    /**
-     * @return string|null
-     */
     public function getZipCode(): ?string
     {
         return $this->zipCode;
     }
-
-    /**
-     * @param string|null $zipCode
-     */
-    public function setZipCode(?string $zipCode)
+    public function setZipCode(string $zipCode)
     {
         $this->zipCode = $zipCode;
     }
 
-    /**
-     * @return string|null
-     */
     public function getCountryCode(): ?string
     {
         return $this->countryCode;
     }
-
-    /**
-     * @param string|null $countryCode
-     */
-    public function setCountryCode(?string $countryCode)
+    public function setCountryCode(string $countryCode)
     {
         $this->countryCode = $countryCode;
     }
 
-    /**
-     * @return string|null
-     */
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
-    /**
-     * @param string|null $email
-     */
-    public function setEmail(?string $email)
+    public function setEmail(string $email)
     {
         $this->email = $email;
     }
 
-    /**
-     * @return string|null
-     */
     public function getTelephone(): ?string
     {
         return $this->telephone;
     }
-
-    /**
-     * @param string|null $telephone
-     */
-    public function setTelephone(?string $telephone)
+    public function setTelephone(string $telephone)
     {
         $this->telephone = $telephone;
     }
 
-    /**
-     * @return string|null
-     */
     public function getFax(): ?string
     {
         return $this->fax;
     }
-
-    /**
-     * @param string|null $fax
-     */
-    public function setFax(?string $fax)
+    public function setFax(string $fax)
     {
         $this->fax = $fax;
     }
 
-    /**
-     * @return array
-     */
     public function getImages(): array
     {
         return $this->images;
     }
-
-    /**
-     * @param array $images
-     */
     public function setImages(array $images)
     {
         $this->images = $images;
     }
 
-    /**
-     * @return string|null
-     */
     public function getPageTracking(): ?string
     {
         return $this->pageTracking;
     }
 
-    /**
-     * @param string|null $pageTracking
-     */
-    public function setPageTracking(?string $pageTracking)
+    public function setPageTracking(string $pageTracking)
     {
         $this->pageTracking = $pageTracking;
     }
 
-    /**
-     * @return string|null
-     */
     public function getEventTracking(): ?string
     {
         return $this->eventTracking;
     }
-
-    /**
-     * @param string|null $eventTracking
-     */
-    public function setEventTracking(?string $eventTracking)
+    public function setEventTracking(string $eventTracking)
     {
         $this->eventTracking = $eventTracking;
     }
 
-    /**
-     * @return DateTime
-     */
     public function getUpdatedAt(): DateTime
     {
         return $this->updatedAt;
     }
-
-    /**
-     * @param DateTime $updatedAt
-     */
     public function setUpdatedAt(DateTime $updatedAt)
     {
         $this->updatedAt = $updatedAt;
     }
 
-    /**
-     * @return string
-     */
-    public function getGaLiveStreamUrl(): string
+    public function getGaLiveStreamUrl(): ?string
     {
         return $this->gaLiveStreamUrl;
     }
-
-    /**
-     * @param string $gaLiveStreamUrl
-     */
     public function setGaLiveStreamUrl(string $gaLiveStreamUrl): void
     {
         $this->gaLiveStreamUrl = $gaLiveStreamUrl;
     }
 
-    /**
-     * @return bool
-     */
     public function isDisplayLogoOverlay(): bool
     {
         return $this->displayLogoOverlay;
     }
-
-    /**
-     * @param bool $displayLogoOverlay
-     */
     public function setDisplayLogoOverlay(bool $displayLogoOverlay): void
     {
         $this->displayLogoOverlay = $displayLogoOverlay;
     }
 
-    /**
-     * @return string
-     */
-    public function getGaLiveStreamFeedCid(): string
+    public function getGaLiveStreamFeedCid(): ?string
     {
         return $this->gaLiveStreamFeedCid;
     }
-
-    /**
-     * @param string $gaLiveStreamFeedCid
-     */
     public function setGaLiveStreamFeedCid(string $gaLiveStreamFeedCid): void
     {
         $this->gaLiveStreamFeedCid = $gaLiveStreamFeedCid;
     }
 
-    /**
-     * @return string
-     */
-    public function getPlayerCode(): string
+    public function getPlayerCode(): ?string
     {
         return $this->playerCode;
     }
-
-    /**
-     * @param string $playerCode
-     */
     public function setPlayerCode(string $playerCode): void
     {
         $this->playerCode = $playerCode;
@@ -734,10 +444,6 @@ class Station extends EntityBase
     {
         return $this->livestreamFeeds;
     }
-
-    /**
-     * @param array $livestreamFeeds
-     */
     public function setLivestreamFeeds(array $livestreamFeeds): void
     {
         foreach ($livestreamFeeds as $livestreamFeed) {
